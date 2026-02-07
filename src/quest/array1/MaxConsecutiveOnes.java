a884bf2bf724ebb0feb5de2ba2ad66b8257bb438
kellyihyeon <kellykang13@gmail.com>
2026-02-07 23:09:20 +0900

    Feat: [Array] Max Consecutive Ones 퀘스트 추가

---
+++ src/quest/array1/MaxConsecutiveOnes.java
@@ -0,0 +1,20 @@
+package quest.array1;
+
+public class MaxConsecutiveOnes {
+
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max = 0;
+        int count = 0;
+
+        for (int num : nums) {
+            if (num == 1) {
+                count++;
+            } else {
+                max = Math.max(max, count);
+                count = 0;
+            }
+        }
+
+        return Math.max(max, count);
+    }
+}
