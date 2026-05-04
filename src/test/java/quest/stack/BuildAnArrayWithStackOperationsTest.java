bc61e5badc8828622afb4e084c5bb93c2e0228cd
kellyihyeon <kellykang13@gmail.com>
2026-05-05 00:17:29 +0900

    Feat: [Stack] Build an Array With Stack Operations 퀘스트 추가

---
+++ src/test/java/quest/stack/BuildAnArrayWithStackOperationsTest.java
@@ -0,0 +1,34 @@
+package quest.stack;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class BuildAnArrayWithStackOperationsTest {
+
+    @Test
+    void buildArray_case1() {
+        List<String> actual = new BuildAnArrayWithStackOperations().buildArray(new int[]{1, 3}, 3);
+        List<String> answer = List.of("Push","Push","Pop","Push");
+
+        assertEquals(answer, actual);
+    }
+
+    @Test
+    void buildArray_case2() {
+        List<String> actual = new BuildAnArrayWithStackOperations().buildArray(new int[]{1,2,3}, 3);
+        List<String> answer = List.of("Push","Push","Push");
+
+        assertEquals(answer, actual);
+    }
+
+    @Test
+    void buildArray_case3() {
+        List<String> actual = new BuildAnArrayWithStackOperations().buildArray(new int[]{1, 2}, 4);
+        List<String> answer = List.of("Push","Push");
+
+        assertEquals(answer, actual);
+    }
+
+}
