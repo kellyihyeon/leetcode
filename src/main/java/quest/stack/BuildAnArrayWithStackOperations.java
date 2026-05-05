26dd7bfd2a59df1c8256eab2f9c4a785627c2494
kellyihyeon <kellykang13@gmail.com>
2026-05-05 12:04:54 +0900

    Feat: [Stack] Build an Array With Stack Operations - 간격 계산 기반 최적화 풀이 추가

--- src/main/java/quest/stack/BuildAnArrayWithStackOperations.java
+++ src/main/java/quest/stack/BuildAnArrayWithStackOperations.java
@@ -6,15 +6,24 @@ import java.util.Stack;
 
 public class BuildAnArrayWithStackOperations {
 
-    // * target 은 정렬 처리 되어 있다.
-    // stream 을 만든다. 1부터 n까지 정수 공급용.
-    // stack 에 stream 의 값을 push 한다. (기록)
-    // stack 의 값이 target 과 일치하지 않으면 pop 한다.
-        // 일치하는지 어떻게 판단할 건데?
-        // 인덱스를 increment 해서 하면?
-        // index = 0, target[index] 와 stack 의 top 을 비교한다.
-        // 일치하면 index 를 +1 증가시킨다.
-        // 불일치하면 stack 의 값을 pop 한다. (기록)
+    public List<String> buildArrayByCalculatingGaps(int[] target, int n) {
+        List<String> result = new ArrayList<>();
+        int index = 1;
+
+        for (int num : target) {
+            while (index < num) {
+                result.add("Push");
+                result.add("Pop");
+                index++;
+            }
+
+            result.add("Push");
+            index++;
+        }
+
+        return result;
+    }
+
     public List<String> buildArray(int[] target, int n) {
         List<String> result = new ArrayList<>();
         Stack<Integer> stack = new Stack<>();
--- src/test/java/quest/stack/BuildAnArrayWithStackOperationsTest.java
+++ src/test/java/quest/stack/BuildAnArrayWithStackOperationsTest.java
@@ -8,17 +8,41 @@ import org.junit.jupiter.api.Test;
 class BuildAnArrayWithStackOperationsTest {
 
     @Test
-    void buildArray_case1() {
-        List<String> actual = new BuildAnArrayWithStackOperations().buildArray(new int[]{1, 3}, 3);
+    void buildArrayByCalculatingGaps_case1() {
+        List<String> actual = new BuildAnArrayWithStackOperations().buildArrayByCalculatingGaps(new int[]{1, 3}, 3);
         List<String> answer = List.of("Push","Push","Pop","Push");
 
         assertEquals(answer, actual);
     }
 
     @Test
-    void buildArray_case2() {
-        List<String> actual = new BuildAnArrayWithStackOperations().buildArray(new int[]{1,2,3}, 3);
+    void buildArrayByCalculatingGaps_case2() {
+        List<String> actual = new BuildAnArrayWithStackOperations().buildArrayByCalculatingGaps(new int[]{1,2,3}, 3);
         List<String> answer = List.of("Push","Push","Push");
+
+        assertEquals(answer, actual);
+    }
+
+    @Test
+    void buildArrayByCalculatingGaps_case3() {
+        List<String> actual = new BuildAnArrayWithStackOperations().buildArrayByCalculatingGaps(new int[]{1, 2}, 4);
+        List<String> answer = List.of("Push","Push");
+
+        assertEquals(answer, actual);
+    }
+
+    @Test
+    void buildArray_case1() {
+        List<String> actual = new BuildAnArrayWithStackOperations().buildArray(new int[]{1, 3}, 3);
+        List<String> answer = List.of("Push", "Push", "Pop", "Push");
+
+        assertEquals(answer, actual);
+    }
+
+    @Test
+    void buildArray_case2() {
+        List<String> actual = new BuildAnArrayWithStackOperations().buildArray(new int[]{1, 2, 3}, 3);
+        List<String> answer = List.of("Push", "Push", "Push");
 
         assertEquals(answer, actual);
     }
@@ -26,7 +50,7 @@ class BuildAnArrayWithStackOperationsTest {
     @Test
     void buildArray_case3() {
         List<String> actual = new BuildAnArrayWithStackOperations().buildArray(new int[]{1, 2}, 4);
-        List<String> answer = List.of("Push","Push");
+        List<String> answer = List.of("Push", "Push");
 
         assertEquals(answer, actual);
     }
